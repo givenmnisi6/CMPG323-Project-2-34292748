f917c960f8944fe880c2c0eab7d72676a4205ccd
GIVEN MNISI <givenmnisi6@gmail.com>
2022-09-01 13:35:02 +0200

    Updated Project

---
+++ CMPG323-Project-2-34292748/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMPG323_Project_2_34292748.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
