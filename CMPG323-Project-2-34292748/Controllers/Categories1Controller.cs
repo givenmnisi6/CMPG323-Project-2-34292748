20fd350cf1dc312d8f7e43781fc6d075e501257f
GIVEN MNISI <givenmnisi6@gmail.com>
2022-09-01 13:52:58 +0200

    New Controller

---
+++ CMPG323-Project-2-34292748/Controllers/Categories1Controller.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CMPG323_Project_2_34292748.Models;
+
+namespace CMPG323_Project_2_34292748.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Categories1Controller : ControllerBase
+    {
+        private readonly ConnectedOfficedbContext _context;
+
+        public Categories1Controller(ConnectedOfficedbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categories1
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategory()
+        {
+            return await _context.Category.ToListAsync();
+        }
+
+        // GET: api/Categories1/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(Guid id)
+        {
+            var category = await _context.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // PUT: api/Categories1/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(Guid id, Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categories1
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory(Category category)
+        {
+            _context.Category.Add(category);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CategoryExists(category.CategoryId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCategory", new { id = category.CategoryId }, category);
+        }
+
+        // DELETE: api/Categories1/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Category>> DeleteCategory(Guid id)
+        {
+            var category = await _context.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Category.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        private bool CategoryExists(Guid id)
+        {
+            return _context.Category.Any(e => e.CategoryId == id);
+        }
+    }
+}
